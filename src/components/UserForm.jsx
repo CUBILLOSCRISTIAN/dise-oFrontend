8e048fb76836360e98676c3cd7889b595172c27b
CUBILLOSCRISTIAN <cccubillos@uninorte.edu.co>
2024-05-16 15:12:56 -0500

    feat: Add MailIcon and LockIcon components

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Modal,
   ModalContent,
@@ -7,16 +7,14 @@ import {
   ModalFooter,
   Button,
   useDisclosure,
-  Checkbox,
   Input,
-  Link,
   DatePicker,
   Select,
   SelectItem,
+  Progress,
 } from "@nextui-org/react";
 import { PlusIcon } from "../icons/PlusIcon.jsx";
 import { MailIcon } from "../icons/MailIcon.jsx";
-import { LockIcon } from "../icons/LockIcon.jsx";
 
 // Definimos el componente de la aplicación
 const UserForm = () => {
@@ -26,12 +24,14 @@ const UserForm = () => {
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [numberPhone, setNumberPhone] = useState("");
-  const [rememberMe, setRememberMe] = useState(false);
-  // Estado para manejar la foto de perfil
+  const [gender, setGender] = useState("");
   const [photo, setPhoto] = useState(null); // Estado para manejar la foto seleccionada
-  const [photoError, setPhotoError] = useState(""); // Estado para manejar errores de la foto
   const [documentType, setDocumentType] = useState("");
   const [numberDocument, setNumberDocument] = useState("");
+  const [birthDay, setBirthDay] = useState(null);
+
+  const [progress, setProgress] = React.useState(0);
+  const [photoError, setPhotoError] = useState(""); // Estado para manejar errores de la foto
   // Función para manejar la selección de la foto
   const handlePhotoChange = (e) => {
     const file = e.target.files[0];
@@ -56,10 +56,34 @@ const UserForm = () => {
     onClose();
   };
 
-  // Función para manejar el cambio del estado de "recordarme"
-  const handleRememberMeChange = () => {
-    setRememberMe(!rememberMe);
-  };
+  useEffect(() => {
+    const calculateProgress = () => {
+      let completedFields = 0;
+      if (firstName) completedFields++;
+      if (secondName) completedFields++;
+      if (lastName) completedFields++;
+      if (email) completedFields++;
+      if (numberPhone) completedFields++;
+      if (gender) completedFields++;
+      if (photo) completedFields++;
+      if (documentType) completedFields++;
+      if (numberDocument) completedFields++;
+      if (birthDay) completedFields++;
+      setProgress(completedFields * 10);
+    };
+    calculateProgress();
+  }, [
+    firstName,
+    secondName,
+    lastName,
+    email,
+    numberPhone,
+    gender,
+    photo,
+    documentType,
+    numberDocument,
+    birthDay,
+  ]);
 
   return (
     <>
@@ -73,7 +97,7 @@ const UserForm = () => {
         Add New
       </Button>
 
-      {/* Modal para el formulario de inicio de sesión */}
+      {/* Modal para el formulario de crear usuario */}
       <Modal
         isOpen={isOpen}
         onClose={onClose}
@@ -83,7 +107,17 @@ const UserForm = () => {
       >
         <ModalContent>
           {/* Encabezado del modal */}
-          <ModalHeader className="flex flex-col gap-1">Log in</ModalHeader>
+          <ModalHeader className="flex flex-col gap-1">
+            Create User
+            <Progress
+              aria-label="Downloading..."
+              size="md"
+              value={progress}
+              color="success"
+              showValueLabel={true}
+              className="max-w-md"
+            />
+          </ModalHeader>
 
           {/* Cuerpo del modal */}
           <ModalBody>
@@ -96,64 +130,107 @@ const UserForm = () => {
                 type="file"
                 className="file-input"
                 onChange={handlePhotoChange}
+                accept="image/*"
               />
             </label>
-            {photoError && <p className="error-message">{photoError}</p>}
+            {photoError && <p className="text-red-700">{photoError}</p>}
 
             {/* Campo de entrada para el primer nombre */}
             <Input
               value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
+              onChange={(e) => {
+                setFirstName(e.target.value);
+              }}
               label="First Name"
               placeholder="Enter your first name"
               variant="bordered"
               maxLength={30}
-              error={firstName.length > 30}
+              isRequired
             />
 
             {/* Campo de entrada para el segundo nombre */}
             <Input
               value={secondName}
-              onChange={(e) => setSecondName(e.target.value)}
+              onChange={(e) => {
+                setSecondName(e.target.value);
+              }}
               label="Second Name"
               placeholder="Enter your Second name"
               variant="bordered"
               maxLength={30}
-              error={firstName.length > 30}
+              isRequired
             />
 
             {/* Campo de entrada para el apellido */}
             <Input
               value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
+              onChange={(e) => {
+                setLastName(e.target.value);
+              }}
               label="Last Name"
               placeholder="Enter your last name"
               variant="bordered"
               maxLength={60}
-              error={lastName.length > 60}
+              isRequired
             />
+
+            {/* Campo de selección para el tipo de documento */}
+            <Select
+              label="Gender"
+              value={gender}
+              variant="bordered"
+              onChange={(value) => {
+                setGender(value);
+              }}
+              isRequired
+            >
+              <SelectItem value="F">Femenino</SelectItem>
+              <SelectItem value="M">Masculino</SelectItem>
+              <SelectItem value="NB">No binario</SelectItem>
+              <SelectItem value="PNR">Prefiero no reportar</SelectItem>
+            </Select>
 
             {/* Campo de entrada para el correo electrónico */}
             <Input
               value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              endContent={
+                <MailIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+              }
               label="Email"
               placeholder="Enter your email"
               variant="bordered"
-              error={!/^\S+@\S+\.\S+$/.test(email)}
+              isRequired
             />
 
             {/* Campo de entrada para la fecha de nacimiento */}
-            <DatePicker label="Birth date" isRequired variant="bordered" />
+            <DatePicker
+              label="Birth date"
+              value={birthDay}
+              onChange={(value) => {
+                setBirthDay(value);
+              }}
+              isRequired
+              variant="bordered"
+              showMonthAndYearPickers
+            />
 
             {/* Campo de entrada para el celular */}
             <Input
               value={numberPhone}
               type="number"
-              onChange={(e) => setNumberPhone(e.target.value)}
+              onChange={(e) => {
+                const value = e.target.value;
+                if (/^\d*$/.test(value) && value.length <= 10) {
+                  setNumberPhone(value);
+                }
+              }}
               label="Number Phone"
               placeholder="Enter your number phone"
               variant="bordered"
+              isRequired
               startContent={
                 <div className="pointer-events-none flex items-center">
                   <span className="text-default-400 text-small">+57 </span>
@@ -166,7 +243,10 @@ const UserForm = () => {
               label="Document Type"
               value={documentType}
               variant="bordered"
-              onChange={(value) => setDocumentType(value)}
+              isRequired
+              onChange={(value) => {
+                setDocumentType(value);
+              }}
             >
               <SelectItem value="TI">Tarjeta de Identidad</SelectItem>
               <SelectItem value="CC">Cédula</SelectItem>
@@ -176,33 +256,35 @@ const UserForm = () => {
             <Input
               value={numberDocument}
               type="number"
-              onChange={(e) => setNumberDocument(e.target.value)}
+              onChange={(e) => {
+                const value = e.target.value;
+                if (/^\d*$/.test(value) && value.length <= 10) {
+                  setNumberDocument(value);
+                }
+              }}
+              isRequired
               label="Number Document"
               placeholder="Enter your number of document"
               variant="bordered"
-              maxLength={5}
             />
-
-            {/* Checkbox para "recordarme" */}
-            <div className="flex py-2 px-1 justify-between">
-              <Checkbox
-                checked={rememberMe}
-                onChange={handleRememberMeChange}
-                classNames={{
-                  label: "text-small",
-                }}
-              >
-                Remember me
-              </Checkbox>
-              {/* Enlace para "¿Olvidó su contraseña?" */}
-              <Link color="primary" href="#" size="sm">
-                Forgot password?
-              </Link>
-            </div>
           </ModalBody>
 
           {/* Pie del modal */}
           <ModalFooter>
+            {/* Checkbox para "dar info" */}
+            <div
+              className="flex items-center text-blue-500 text-sm font-bold px-4 py-3"
+              role="alert"
+            >
+              <svg
+                className="fill-current w-4 h-4 mr-2"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <path d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z" />
+              </svg>
+              <p>All fiels are required</p>
+            </div>
             {/* Botón para cerrar el modal */}
             <Button color="danger" variant="flat" onClick={onClose}>
               Close
@@ -217,7 +299,13 @@ const UserForm = () => {
                 !email ||
                 !/^\S+@\S+\.\S+$/.test(email) ||
                 firstName.length > 30 ||
-                lastName.length > 60
+                lastName.length > 60 ||
+                !photo ||
+                photoError ||
+                !numberPhone ||
+                numberPhone.length !== 10 ||
+                !documentType ||
+                !numberDocument
               }
             >
               Create User
