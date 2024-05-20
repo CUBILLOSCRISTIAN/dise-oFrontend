f6f1365c944b197077441e574b2e1ea04c1f1702
CUBILLOSCRISTIAN <cccubillos@uninorte.edu.co>
2024-05-20 16:19:22 -0500

    chore: Update RenderCell component to include documentType, gender, numberDocument, and birthDay options
    The code changes in this commit update the RenderCell component to include options for rendering the documentType, gender, numberDocument, and birthDay values. This enhancement improves the user experience by displaying relevant information in a more user-friendly format, such as using icons for document types and displaying birth dates in a readable format.

--- src/components/UserDashboard/RenderCell.jsx
+++ src/components/UserDashboard/RenderCell.jsx
@@ -10,10 +10,9 @@ import {
 } from "@nextui-org/react";
 import { statusColorMap } from "../../data";
 import ActionDropdown from "./ActionDropdown";
-import { EditIcon } from "../../icons/EditIcon";
-import { DeleteIcon } from "../../icons/DeleteIcon";
-import { EyeIcon } from "../../icons/EyeIcon";
 import { UserPopoverCard } from "./UserPopoverCard";
+import { FaRegAddressCard } from "react-icons/fa";
+import { HiCalendar, HiPhone } from "react-icons/hi";
 
 const RenderCell = (user, columnKey) => {
   const cellValue = user[columnKey];
@@ -56,6 +55,61 @@ const RenderCell = (user, columnKey) => {
           <ActionDropdown user={user} />
         </div>
       );
+    case "documentType": {
+      if (cellValue === "CC") {
+        return (
+          <Chip startContent={<FaRegAddressCard size={18} />} variant="light">
+            Cedula de ciudadanía
+          </Chip>
+        );
+      } else {
+        return (
+          <Chip startContent={<FaRegAddressCard size={18} />} variant="light">
+            Tarjeta de identidad
+          </Chip>
+        );
+      }
+    }
+    case "gender": {
+      if (cellValue === "M") {
+        return <Chip variant="light">Masculino</Chip>;
+      }
+      if (cellValue === "F") {
+        return <Chip variant="light">Femenino</Chip>;
+      }
+      if (cellValue === "NB") {
+        return <Chip variant="light">No binario</Chip>;
+      }
+      if (cellValue === "PNR") {
+        return <Chip variant="light">Prefiero no responder</Chip>;
+      }
+    }
+    case "numberDocument": {
+      if (user.documentType === "CC") {
+        return (
+          <div className="flex flex-col">
+            <p className="text-bold text-sm capitalize">Cedula de ciudadanía</p>
+            <p className="text-bold text-sm capitalize text-default-400">
+              {cellValue}
+            </p>
+          </div>
+        );
+      } else {
+        return (
+          <div className="flex flex-col">
+            <p className="text-bold text-sm capitalize">Tarjeta de identidad</p>
+            <p className="text-bold text-sm capitalize text-default-400">
+              {cellValue}
+            </p>
+          </div>
+        );
+      }
+    }
+    case "birthDay":
+      return (
+        <Chip variant="light">{new Date(cellValue).toLocaleDateString()}</Chip>
+      );
+
     default:
       return cellValue;
   }
