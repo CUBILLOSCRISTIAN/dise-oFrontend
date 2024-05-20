00b0346e93551c0798aef0d8bbcf0cbb9e121556
CUBILLOSCRISTIAN <cccubillos@uninorte.edu.co>
2024-05-20 15:27:12 -0500

    chore: Update ActionDropdown component to include user details modal
    The code changes in this commit update the ActionDropdown component to include a user details modal. This modal is triggered when the "View" option is selected from the dropdown menu. It fetches the user details from the server and displays them in a modal card layout using the UserModalCard component. This enhancement improves the user experience by providing a convenient way to view user details without navigating away from the current page.

--- src/components/UserDashboard/ActionDropdown.jsx
+++ src/components/UserDashboard/ActionDropdown.jsx
@@ -1,20 +1,82 @@
 import React from "react";
-import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button } from "@nextui-org/react";
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+  Button,
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+} from "@nextui-org/react";
 import { VerticalDotsIcon } from "../../icons/VerticalDotsIcon";
+import { UserModalCard } from "./UserModalCard";
 
-const ActionDropdown = () => (
-  <Dropdown className="bg-background border-1 border-default-200">
-    <DropdownTrigger>
-      <Button isIconOnly radius="full" size="sm" variant="light">
-        <VerticalDotsIcon className="text-default-400" />
-      </Button>
-    </DropdownTrigger>
-    <DropdownMenu>
-      <DropdownItem>View</DropdownItem>
-      <DropdownItem>Edit</DropdownItem>
-      <DropdownItem>Delete</DropdownItem>
-    </DropdownMenu>
-  </Dropdown>
-);
+const ActionDropdown = (item) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [user, setUser] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+
+  const handleAction = async (key) => {
+    console.log(item.user._id);
+    if (key === "view") {
+      try {
+        const response = await fetch(
+          `http://localhost:3004/users/${item.user._id}`
+        );
+        const data = await response.json();
+        setUser(data);
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+      }
+      onOpen();
+    }
+  };
+
+  return (
+    <>
+      <Dropdown className="bg-background border-1 border-default-200">
+        <DropdownTrigger>
+          <Button isIconOnly radius="full" size="sm" variant="light">
+            <VerticalDotsIcon className="text-default-400" />
+          </Button>
+        </DropdownTrigger>
+        <DropdownMenu
+          aria-label="Action event"
+          onAction={(key) => handleAction(key)}
+        >
+          <DropdownItem key="view">View</DropdownItem>
+          <DropdownItem key="edit">Edit</DropdownItem>
+          <DropdownItem key="delete" color="danger" className="text-danger">
+            Delete
+          </DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+      <Modal size="lg" isOpen={isOpen} onClose={onClose} backdrop="blur">
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                View user details
+              </ModalHeader>
+              <ModalBody>
+                <UserModalCard user={user} onClose={onClose} />
+              </ModalBody>
+              <ModalFooter>
+                <Button auto onClick={onClose}>
+                  Close
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
 
 export default ActionDropdown;
--- src/components/UserDashboard/RenderCell.jsx
+++ src/components/UserDashboard/RenderCell.jsx
@@ -52,22 +52,8 @@ const RenderCell = (user, columnKey) => {
       );
     case "actions":
       return (
-        <div className="relative flex items-center gap-2">
-          <Tooltip content="Details">
-            <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-              <EyeIcon />
-            </span>
-          </Tooltip>
-          <Tooltip content="Edit user">
-            <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-              <EditIcon />
-            </span>
-          </Tooltip>
-          <Tooltip color="danger" content="Delete user">
-            <span className="text-lg text-danger cursor-pointer active:opacity-50">
-              <DeleteIcon />
-            </span>
-          </Tooltip>
+        <div className="relative flex justify-end items-center gap-2">
+          <ActionDropdown user={user} />
         </div>
       );
     default:
